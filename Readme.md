3d159349f94fc7f2584af5a6f0583a4cf4eb502d
Rudhin Menon <menonrudhin@gmail.com>
2025-01-02 15:12:51 -0500

    deployable to azure function app

--- Readme.md
+++ Readme.md
@@ -1,8 +1,6 @@
-# Setup Flask
-1. python3 -m venv .venv
-1. . .venv/bin/activate
-1. pip3 install Flask
-# Start the flask app
-1. python3 exchange_rate.py
-# Test in browser
-http://127.0.0.1:5000/fx-rate?from_ccy=CAD&to_ccy=USD&amount=100
+# How to deploy manually
+1. func azure functionapp publish CcyConvertTrigger
+1. curl -X GET -H 'x-functions-key:<API-KEY-HERE>' https://ccyconverttrigger.azurewebsites.net/api/ccyconverttrigger\?from_ccy=JPY\&to_ccy=GBP\&amount=63
+# How to test locally
+1. func start
+1. curl -X GET http://localhost:7071/api/CcyConvertTrigger?from_ccy=GBP&to_ccy=USD&amount=15
---
+++ function_app.py
@@ -0,0 +1,36 @@
+import azure.functions as func
+import datetime
+import json
+import logging
+import urllib.request
+import re
+
+app = func.FunctionApp()
+pattern = r'class="BNeawe iBp4i AP7Wnd">[0-9,]+.?[0-9]* [a-zA-Z ]+<\/div>'
+
+@app.route(route="CcyConvertTrigger", auth_level=func.AuthLevel.FUNCTION)
+def CcyConvertTrigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    #name = req.params.get('name')
+
+    FROM_CCY=req.params.get('from_ccy')
+    TO_CCY=req.params.get('to_ccy')
+    AMOUNT=req.params.get('amount')
+
+    if FROM_CCY and TO_CCY and AMOUNT:
+        result = urllib.request.urlopen(f'https://www.google.com/search?q={AMOUNT}+{FROM_CCY}+to+{TO_CCY}&oq={AMOUNT}+{FROM_CCY}+to+{TO_CCY}');
+        raw_text = str(result.read());
+        matches = re.finditer(pattern, raw_text)
+        for match in matches:
+            start = match.start()
+            end = raw_text.index('<',start)
+
+            ret_val = raw_text[start + 28:end]
+            print(ret_val)
+        return func.HttpResponse(f"{ret_val}")
+    else:
+        return func.HttpResponse(
+             "Pass from_ccy, to_ccy, and amount as parameters",
+             status_code=200
+        )
